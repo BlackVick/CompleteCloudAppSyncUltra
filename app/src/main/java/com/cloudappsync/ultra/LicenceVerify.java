135d8bd21736ba1026e96d9d04374be7d5929000
ESW <adesuyiayo@rocketmail.com>
2021-07-08 04:51:47 +0100

    Removed verbose internet check

--- app/src/main/java/com/cloudappsync/ultra/LicenceVerify.java
+++ app/src/main/java/com/cloudappsync/ultra/LicenceVerify.java
@@ -104,6 +104,7 @@ public class LicenceVerify extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_REQUEST_CODE) {
 
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
